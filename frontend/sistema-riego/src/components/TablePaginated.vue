9087647e22c7c60398e9e0bf28a445b21432bdba
Matías García Gimeno <mgg709@inlumine.ual.es>
2023-08-29 19:13:02 +0200

    tabla con paginacion y obteniendo datos

---
+++ frontend/sistema-riego/src/components/TablePaginated.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="btn-group">
+    <button @click="emits('previous')" :disabled=" inicio <= 0 ? true : false" class="left-button">Anterior</button>
+    <button @click="emits('next')" :disabled="fin >= longitud  ? true : false " class="right-button">Siguiente</button>
+  </div>
+</template>
+<script setup>
+
+const props = defineProps({
+  inicio: {
+    type: Number,
+    required: true
+  },
+  fin: {
+    type: Number,
+    required: true
+  },
+  longitud: {
+    type: Number,
+    required: true
+  }
+})
+const emits = defineEmits(['previous', 'next'])
+
+</script>
+<style scoped></style>
--- frontend/sistema-riego/src/views/HomeView.vue
+++ frontend/sistema-riego/src/views/HomeView.vue
@@ -1,7 +1,106 @@
 <template>
   <div class="home-content">
-    
+
+    <h1>SISTEMA DE GESTIÓN DE RIEGO</h1>
+    <span>Buscador</span>
+
+    <TablePaginated @next="next" @previous="previous" :inicio="inicio" :fin="fin" :longitud="maxLength"/>
+
+    <table class="clients-table">
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Cliente</th>
+          <th>Fecha</th>
+          <th>Direccion</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="cliente in clientes.slice(inicio, fin)" :key="cliente.id">
+          <td>{{ cliente.codigo }}</td>
+          <td>{{ cliente.razon_social }}</td>
+          <td>{{ cliente.cif }}</td>
+          <td>{{ cliente.direccion }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 <script setup>
+import { computed, onMounted, ref } from 'vue';
+import TablePaginated from '../components/TablePaginated.vue';
+import axios from 'axios';
+
+const clienteXpage = 10;
+const inicio = ref(0);
+const fin = ref(clienteXpage);
+const clientes = ref([]);
+let maxLength = computed(() => clientes.value.length);
+
+
+const next = () => {
+  inicio.value += clienteXpage;
+  fin.value += clienteXpage;
+}
+
+const previous = () => {
+  inicio.value -= clienteXpage;
+  fin.value -= clienteXpage;
+}
+
+async function getData(){
+  const { data } = await axios.get('http://localhost/api/clientes'); 
+  clientes.value = data;
+  console.log(clientes.value);
+}
+
+onMounted(getData);
 </script>
+
+<style scoped>
+.home-content{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.data{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.clients-table{
+  width: 70%;
+  border-collapse: collapse;
+  margin: 25px 0;
+  font-size: 0.9em;
+  min-width: 400px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+}
+
+.clients-table thead tr {
+  background-color: #009879;
+  color: #ffffff;
+  text-align: left;
+}
+
+.clients-table th,
+.clients-table td {
+  padding: 12px 15px;
+}
+
+.clients-table tbody tr {
+  border-bottom: 1px solid #dddddd;
+}
+
+.clients-table tbody tr:nth-of-type(even) {
+  background-color: #f3f3f3;
+}
+
+.clients-table tbody tr:last-of-type {
+  border-bottom: 2px solid #009879;
+}
+</style>
